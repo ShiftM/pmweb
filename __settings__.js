b3a165a471d46a92811c2953903ae54d90716153
ShiftM <57879243+ShiftM@users.noreply.github.com>
2020-07-22 20:31:49 +0800

    First Commit
    First Commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "956728.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 33099072, 33100091, 33101087, 33126447, 33129206, 33301866, 33301867, 33339428, 33352585 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/33101070/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/33101071/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/33101069/1/ammo.js', 'preload' : true},
+];
